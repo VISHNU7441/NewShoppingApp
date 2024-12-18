fcac87e754b22ed47627c13a0af643cf09fda759
VISHNU7441 <vishnurs74417441@gmail.com>
2024-12-18 17:53:17 +0300

    Create separate model for managing cart as singleton
    [Clean] New CartManager for handling cart

---
+++ NewShoppingApp/Services/CartManager.swift
@@ -0,0 +1,38 @@
+//
+//  CartManager.swift
+//  NewShoppingApp
+//
+//  Created by vishnu r s on 29/11/24.
+//
+
+import Foundation
+
+class CartManager:ObservableObject{
+    
+    static let shared = CartManager()
+    @Published var cartItems:[CartItem] = []
+    
+    private init(){ }
+    
+    
+    func addToCart(product:Product){
+        if cartItems.contains(where: { $0.product.id == product.id}){
+            return
+        }else{
+            let cartItem = CartItem(product: product, quantity: 1)
+            cartItems.append(cartItem)
+        }
+    }
+    
+    func deleteCartItem(product:Product){
+        cartItems.removeAll { CartItem in
+            CartItem.id == product.id
+        }
+    }
+    
+    func incrementQuantity(product:Product){
+        if let index = cartItems.firstIndex(where: { $0.id == product.id}){
+            cartItems[index].quantity += 1
+        }
+    }
+}
